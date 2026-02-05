4bfbaf1dba9ec71ed74baea59559ec38e04bdb15
ongudidan <ongudidan@gmail.com>
2026-02-05 10:40:31 +0300

    feat: Package UFW plugin files locally and update installation script to copy them instead of downloading.

--- resources/extra/ufw_int.sh
+++ resources/extra/ufw_int.sh
@@ -1,7 +1,10 @@
-wget -O /usr/local/ufw.zip "https://olspanel.com/plugin/ufw.zip?$(date +%s)"
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+PLUGIN_DIR="$SCRIPT_DIR/../plugins"
+
+cp "$PLUGIN_DIR/ufw.zip" /usr/local/ufw.zip
 sudo unzip -o /usr/local/ufw.zip -d /usr/local
 
-wget -O /usr/local/config_ufw.zip "https://olspanel.com/plugin/config_ufw.zip?$(date +%s)"
+cp "$PLUGIN_DIR/config_ufw.zip" /usr/local/config_ufw.zip
 
 if [ ! -d "/usr/local/ufw/conf" ]; then
     sudo unzip -o /usr/local/config_ufw.zip -d /usr/local/ufw
